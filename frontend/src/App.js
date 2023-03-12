ff4968a62b54a91896ab079f5068b55f8df3a622
Quy <dangduyquy@outlook.com>
2023-03-12 23:24:29 +0700

    first request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,28 @@ import Header from './components/Header';
 import Search from './components/Search';
 
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
+
 
 const App = () => {
-  const [word, setWord] = useState()
+  const [word, setWord] = useState('')
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
+  // console.log(process.env.REACT_APP_UNSPLASH_KEY)
 
+ 
   return (
     <div>
       <Header title = "Images Gallery" />
@@ -22,3 +35,29 @@ const App = () => {
 }
 
 export default App;
+
+export const userPostFetch = user => {
+  return dispatch => {
+    return fetch("http://localhost:3000/api/v1/users", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({user})
+    })
+      .then(resp => resp.json())
+      .then(data => {
+        if (data.message) {
+        } else {
+          localStorage.setItem("token", data.jwt)
+          dispatch(loginUser(data.user))
+        }
+      })
+  }
+}
+
+const loginUser = userObj => ({
+    type: 'LOGIN_USER',
+    payload: userObj
+})
